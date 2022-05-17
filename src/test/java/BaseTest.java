7533042324e3559e0ef9caca66b250044875c785
ym021 <ym021@hdm-stutgart.de>
2022-05-17 16:45:06 +0200

    add dummy test for checking mvn test on the ci

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ */
+class BaseTest {
+
+    @Test
+    void test(){
+        Assertions.assertTrue(true);
+    }
+}
